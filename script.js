111a3e90e297e49cb07de0569620e4ed379c1595
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-06-23 18:41:52 +0530

    integrate bootstrap icons

--- script.js
+++ script.js
@@ -73,3 +73,9 @@ const observer = new IntersectionObserver(entries => {
 })
 
 document.querySelectorAll('.hidden').forEach(el => observer.observe(el));
+
+/*
+document.querySelector('.icon').addEventListener('click', () => {
+	document.querySelector('nav').classList.toggle('responsive');
+});
+*/

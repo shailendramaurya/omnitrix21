66405708bf72f0d8ee3d1c8364415f88decbb81a
Aarav <122670016+omnitrix21@users.noreply.github.com>
2023-07-24 18:28:35 +0530

    fixup and changes

--- type.js
+++ type.js
@@ -1,6 +1,6 @@
 // type.js
 
-function typeWriterEffect(element, text, speed = 100) {
+function typeWriterEffect(element, text, speed = 70) {
   let i = 0;
   const typing = () => {
     if (i < text.length) {
@@ -15,10 +15,6 @@ function typeWriterEffect(element, text, speed = 100) {
 // Call the function when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', () => {
   const heading = document.querySelector('.typing-heading');
-  const text = "Welcome to BurhanVerse";
-  typeWriterEffect(heading, text, 100); // Adjust the speed as needed
-
-  const subHeading = document.querySelector('.typing-subheading');
-  const subText = "Welcome to ElectroKernel for Sweet.\nBy Team Electro";
+  const text = "Welcome to ElectroKernel for Sweet.\nBy Team Electro";
   typeWriterEffect(subHeading, subText, 100); // Adjust the speed as needed
 });

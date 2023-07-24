205028ca828161ac39f584c0be53cbbb10397a3d
Aarav <122670016+omnitrix21@users.noreply.github.com>
2023-07-24 17:14:39 +0530

    new scrollAnimation.js

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -1,10 +1,22 @@
 
-const observer = new IntersectionObserver(entries => {
-	entries.forEach(entry => {
-		entry.isIntersecting ?
-			entry.target.classList.add('show') :
-			entry.target.classList.remove('show');
-	})
-})
+// scrollAnimation.js
 
+const observerOptions = {
+	root: null, // Use the viewport as the root
+	rootMargin: '0px', // No root margin
+	threshold: 0.2, // Triggers the animation when 20% of the element is visible
+};
+
+const handleIntersect = (entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+			observer.unobserve(entry.target); // Remove the observer after the animation is triggered
+		}
+	});
+};
+
+const observer = new IntersectionObserver(handleIntersect, observerOptions);
+
+// Observe all elements with the class 'hidden'
 document.querySelectorAll('.hidden').forEach(el => observer.observe(el));
